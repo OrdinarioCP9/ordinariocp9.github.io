a4082f16fd6caec8e35973d20d0e6186317e6d82
Erlyn Pino <erlynpino@gmail.com>
2025-03-31 09:24:28 -0500

    Actualizar la ruta de importación del archivo CSS en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'C:/Users/ordin/Documents/Proyectos web/ordinariocp9.github.io/index.css'
+import '../index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
